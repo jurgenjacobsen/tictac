1f0f488dbc02f8f5b9394ef03c160f4032e5d6fe
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2025-11-23 21:53:50 +0000

    feat: Update repository name in Electron app and implement system tray functionality

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import { app, BrowserWindow, Tray, Menu, nativeImage } from 'electron';
+import { app, BrowserWindow, Tray, Menu } from 'electron';
 import path from 'node:path';
 import { updateElectronApp } from 'update-electron-app';
 
 updateElectronApp({
-  repo: 'jurgenheinz/tictac',
+  repo: 'jurgenjacobsen/tictac',
   notifyUser: true,
 });
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
+const ICON_PATH: string = path.join(__dirname, '../../src/assets/icon.ico');
+
 
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -19,7 +21,7 @@ let mainWindow: BrowserWindow | null = null;
 const createWindow = (): void => {
   mainWindow = new BrowserWindow({
     title: 'TicTac',
-    icon: path.join(__dirname, 'assets', 'icon.png'),
+    icon: ICON_PATH,
     autoHideMenuBar: true,
     center: true,
     webPreferences: {
@@ -42,19 +44,44 @@ const createWindow = (): void => {
       },
     });
   });
+
 };
+
+const createTray = (): void => {
+  const tray = new Tray(ICON_PATH);
+  tray.setToolTip('TicTac');
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Open App',
+      click: () => {
+        const wins = BrowserWindow.getAllWindows()
+        if (wins.length === 0) {
+          createWindow();
+          mainWindow?.maximize();
+        } else {
+          wins[0].focus()
+        }
+      }
+    },
+    { role: 'quit' }
+  ])
+
+  tray.setContextMenu(contextMenu);
+}
 
 
 app.on('ready', () => {
   createWindow();
+  
+  createTray();
 
   if (mainWindow) mainWindow.maximize();
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  /*if (process.platform !== 'darwin') {
     app.quit();
-  }
+  }*/
 });
 
 app.on('activate', () => {
